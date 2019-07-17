81491cd70648eac5a320a37370eb5837f1a5b4b8
Nick Franken <shnick@gmail.com>
2019-07-17 15:11:41 -0500

    fixed typings file

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,12 @@
-interface R {
-  data(...args: any[]): R;
-  call(_opts?: R.Options): Promise<object>;
-  callSync(_opts?: R.Options): object;
-}
-declare namespace R {
+declare module '@fridgerator/r-script' {
   interface Options {
-    [key: string]: any;
+    [key: string]: any
+  }
+
+  export class R {
+    constructor(path: string, env?: Options);
+    data (...args: any[]): R;
+    call (_opts?: Options): Promise<object>;
+    callSync (_opts?: Options): object | undefined;
   }
 }
-export = R;
