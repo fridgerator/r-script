7febbbebb0d2e64669650dbfef4f7be09159dee6
Nick Franken <shnick@gmail.com>
2019-07-18 10:39:04 -0500

    environment init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as path from 'path'
 import { spawn, spawnSync } from 'child_process'
 
 interface Options {
@@ -11,16 +12,16 @@ export class R {
   idCounter: number
   args: string[]
 
-  constructor(path: string, env?: Options) {
-    let _env = env || process.env
+  constructor(scriptPath: string, opts?: Options) {
+    let _opts = opts || {}
     this.rData = {}
-    this.path = path
-    this.options = {
-      env: Object.assign({}, {DIRNAME: __dirname}, _env),
+    this.path = scriptPath
+    this.options = Object.assign({}, {
+      env: {DIRNAME: __dirname},
       encoding: 'utf8'
-    }
+    }, _opts)
     this.idCounter = 0
-    this.args = ['--vanilla', __dirname + '/R/launch.R']
+    this.args = ['--vanilla', this.options.env.DIRNAME + '/R/launch.R']
   }
 
   private setInputEnv (opts: Options): void {
@@ -34,33 +35,56 @@ export class R {
     return this
   }
 
-  call (_opts?: Options): Promise<object> {
+  call (_opts?: Options): Promise<any> {
     return new Promise((resolve: any, reject: any) => {
       let opts = _opts || {}
       this.setInputEnv(opts)
+      console.log('args : ', this.args)
+      console.log('opts : ', this.options)
+      console.log('call : ', 'Rscript', this.args, this.options)
       let child = spawn('Rscript', this.args, this.options)
       let body = ""
       child.stderr.on('data', d => {
         let msg = d.toString()
-        if (msg.includes('Warning message')) return
+        if (msg.includes('Warning message')) {
+          console.log(msg)
+          return
+        }
         reject(msg)
       })
       child.stdout.on('data', d => body += d)
-      child.on('close', () => resolve(JSON.parse(body)))
+      child.on('close', () => {
+        try {
+          let response = JSON.parse(body)
+          resolve(response) 
+        } catch (e) {
+          reject(body)
+        }
+      })
     })
   }
 
-  callSync (_opts?: Options): object | undefined {
+  callSync (_opts?: Options): any {
     let opts = _opts || {}
     this.setInputEnv(opts)
     let child = spawnSync('Rscript', this.args, this.options)
     if (child.stderr) {
       let msg = child.stderr.toString()
-      if (!msg.includes('Warning message'))
+      if (msg.includes('Warning message')) {
+        console.log(msg)
+      } else {
         throw msg
+      }
     }
     if (child.stdout) {
-      return JSON.parse(child.stdout.toString())
+      let msg = child.stdout.toString()
+      let response: any
+      try {
+        response = JSON.parse(msg)
+      } catch (e) {
+        throw msg
+      }
+      return response
     }
     return undefined
   }
